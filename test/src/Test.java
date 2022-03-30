f3907dd27c0c7c8c56963096e90df90ae6ae9c4f
hfutxc <chenghao2008@tom.com>
2022-03-30 10:00:38 +0800

    tt

--- test/src/Test.java
+++ test/src/Test.java
@@ -5,8 +5,12 @@ public class Test {
         System.out.println("hello world");
         JFrame test = new JFrame("test");
         test.setBounds(100,100,400,300);
+        JButton b = new JButton("test");
+        test.add(b);
+
         test.setVisible(true);
         test.setAlwaysOnTop(true);
         test.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
     }
 }
